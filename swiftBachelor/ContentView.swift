f94cf35902293773d2ea3b4b216cc660e7047a13
Adrian Bien <adribien94@gmail.com>
2020-07-14 15:35:01 +0200

    Initial Commit

---
+++ swiftBachelor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftBachelor
+//
+//  Created by Adrian Bien on 14.07.20.
+//  Copyright © 2020 Adrian Bien. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
